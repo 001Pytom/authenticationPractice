bb3c18b8355afd0e2bcaf58a9c5255353756fe1f
faatihat <tomfaah@gmail.com>
2024-03-20 12:44:03 +0100

    authentication pract

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from "./form";
+
+// const isLoggedIn = false;
+// function App() {
+//   return (
+//     <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Form />}</div>
+//   );
+// }
+
+// another one, challenge
+
+let userIsRegistered = false;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Form isregistered={userIsRegistered} />
     </div>
   );
 }
---
+++ src/form.jsx
@@ -0,0 +1,26 @@
+// function Form() {
+//   return (
+//     <form className="form">
+//       <input type="text" placeholder="Username" />
+//       <input type="password" placeholder="Password" />
+//       <button type="submit">Login</button>
+//     </form>
+//   );
+// }
+
+import React from "react";
+
+function Form(props) {
+  return (
+    <form className="form">
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      {!props.isregistered && (
+        <input type="password" placeholder="Confirm Password" />
+      )}
+      <button type="submit">{props.isregistered ? "Login" : "Register"}</button>
+    </form>
+  );
+}
+
+export default Form;
